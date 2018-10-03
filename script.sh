1d2fe26e6a59e54149872ea36ca99e34fc1a6085
dtauraso <dtauraso@lab04.c.gifted-decker-214823.internal>
2018-10-03 05:35:57 +0000

    Added output for replica 1

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ ab -p payload.json -T application/json -m POST -c 50 -n 10000 -s 100 -r http://3
 
 
 echo "\n" > divider
-echo "12" > replicas
+echo "1" > replicas
 grep -E 'Concurrency Level:' result1 | awk '{print $3}' > concurrency
 grep -E 'Requests per second:' result1 | awk '{print $4}' > requests_per_sec
 grep -E '(longest request)' result1 | awk '{print $2}' > longest_request
@@ -22,7 +22,7 @@ ab -p payload.json -T application/json -m POST -c 100 -n 10000 -s 100 -r http://
 #grep -E 'Concurrency Level:|Requests per second:|(longest request)|98%' > output2
 
 echo "\n" > divider
-echo "12" > replicas
+echo "1" > replicas
 grep -E 'Concurrency Level:' result2 | awk '{print $3}' > concurrency
 grep -E 'Requests per second:' result2 | awk '{print $4}' > requests_per_sec
 grep -E '(longest request)' result2 | awk '{print $2}' > longest_request
@@ -40,7 +40,7 @@ ab -p payload.json -T application/json -m POST -c 500 -n 10000 -s 100 -r http://
 #grep -E 'Concurrency Level:|Requests per second:|(longest request)|98%' > output3
 
 echo "\n" > divider
-echo "12" > replicas
+echo "1" > replicas
 grep -E 'Concurrency Level:' result3 | awk '{print $3}' > concurrency
 grep -E 'Requests per second:' result3 | awk '{print $4}' > requests_per_sec
 grep -E '(longest request)' result3 | awk '{print $2}' > longest_request
@@ -59,7 +59,7 @@ ab -p payload.json -T application/json -m POST -c 1000 -n 10000 -s 100 -r http:/
 #grep -E 'Concurrency Level:|Requests per second:|(longest request)|98%' > output4
 
 echo "\n" > divider
-echo "12" > replicas
+echo "1" > replicas
 grep -E 'Concurrency Level:' result4 | awk '{print $3}' > concurrency
 grep -E 'Requests per second:' result4 | awk '{print $4}' > requests_per_sec
 grep -E '(longest request)' result4 | awk '{print $2}' > longest_request
@@ -78,7 +78,7 @@ ab -p payload.json -T application/json -m POST -c 2000 -n 10000 -s 100 -r http:/
 #grep -E 'Concurrency Level:|Requests per second:|(longest request)|98%' > output5
 
 echo "\n" > divider
-echo "12" > replicas
+echo "1" > replicas
 grep -E 'Concurrency Level:' result5 | awk '{print $3}' > concurrency
 grep -E 'Requests per second:' result5 | awk '{print $4}' > requests_per_sec
 grep -E '(longest request)' result5 | awk '{print $2}' > longest_request
@@ -96,7 +96,7 @@ ab -p payload.json -T application/json -m POST -c 5000 -n 20000 -s 100 -r http:/
 #grep -E 'Concurrency Level:|Requests per second:|(longest request)|98%' > output6
 
 echo "\n" > divider
-echo "12" > replicas
+echo "1" > replicas
 grep -E 'Concurrency Level:' result6 | awk '{print $3}' > concurrency
 grep -E 'Requests per second:' result6 | awk '{print $4}' > requests_per_sec
 grep -E '(longest request)' result6 | awk '{print $2}' > longest_request
@@ -114,7 +114,7 @@ ab -p payload.json -T application/json -m POST -c 10000 -n 30000 -s 100 -r http:
 #grep -E 'Concurrency Level:|Requests per second:|(longest request)|98%' > output7
 
 echo "\n" > divider
-echo "12" > replicas
+echo "1" > replicas
 grep -E 'Concurrency Level:' result7 | awk '{print $3}' > concurrency
 grep -E 'Requests per second:' result7 | awk '{print $4}' > requests_per_sec
 grep -E '(longest request)' result7 | awk '{print $2}' > longest_request
@@ -132,7 +132,7 @@ ab -p payload.json -T application/json -m POST -c 15000 -n 45000 -s 100 -r http:
 #grep -E 'Concurrency Level:|Requests per second:|(longest request)|98%' > output8
 
 echo "\n" > divider
-echo "12" > replicas
+echo "1" > replicas
 grep -E 'Concurrency Level:' result8 | awk '{print $3}' > concurrency
 grep -E 'Requests per second:' result8 | awk '{print $4}' > requests_per_sec
 grep -E '(longest request)' result8 | awk '{print $2}' > longest_request
